6af5f3fa982b3bce2d6d0f0cc68bdfd96410abc4
LAPTOP-7B119G91\髙濵一輝 <itsuchanitsu121010@gmail.com>
2021-07-16 00:25:52 +0900

    探索パート　ステージ移動、敵の処理

---
+++ Assets/Scripts/SearchEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchEnemy : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("ok");
+            collision.gameObject.GetComponent<SearchPlayer>().HitEnemy(this);
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SearchPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchPlayer : MonoBehaviour
+{
+
+    public void HitEnemy(SearchEnemy enemy)
+    {
+        Debug.Log(enemy.gameObject.name);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
